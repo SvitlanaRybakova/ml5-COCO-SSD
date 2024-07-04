fc2e82d83aff0bbcd890ee6de159bc10c069765c
Svitlana Rybakova <rsv0011@gmail.com>
2024-07-04 18:23:34 +0200

    feat: add error boundary, refactor code

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import Webcam from 'react-webcam'
 import './App.css'
 import { useEffect, useRef } from 'react'
+import ErrorBoundary from './components/ErrorBoundary'
+
 
 const dimensions = {
     width: 800,
@@ -8,59 +10,80 @@ const dimensions = {
 }
 
 function App() {
+    const { width, height } = dimensions
+    const detections = useRef([])
     const webcamRef = useRef()
     const canvasRef = useRef()
-    const { width, height } = dimensions
+ 
+
+    const draw = () => {
+        const ctx = canvasRef.current.getContext('2d')
+        ctx.clearRect(0, 0, width, height)
+        detections.current.forEach((detection) => {
+            ctx.beginPath()
+            ctx.fillStyle = '#FF0000'
+            ctx.fillText(detection.label, detection.x, detection.y - 5)
+            ctx.rect(
+                detection.x,
+                detection.y,
+                detection.width,
+                detection.height
+            )
+            ctx.stroke()
+        })
+    }
+
+
+    const detect = (objectDetector) => {
+        console.log(webcamRef.current.video.readyState);
+        if (webcamRef.current.video.readyState !== 4) {
+            console.error('Video not ready yet')
+            return
+        }
+
+        objectDetector.detect(webcamRef.current.video, (err, results) => {
+            if (err) {
+                console.error('Error, detecting the video')
+                return
+            }
+
+            detections.current = results
+            draw()
+        })
+    }
 
     useEffect(() => {
         let detectionInterval
 
         const modelLoaded = () => {
-            webcamRef.current.video.width = width
-            webcamRef.current.video.height = height
-            canvasRef.current.width = width
-            canvasRef.current.height = height
 
+            webcamRef.current.video.width = width;
+            webcamRef.current.video.height = height;
+            canvasRef.current.width = width;
+            canvasRef.current.height = height;
+        }
+        const objectDetector = ml5.objectDetector('cocossd', modelLoaded)
+
+        if (modelLoaded) {
             detectionInterval = setInterval(() => {
-                detect()
+                detect(objectDetector)
             }, 200)
         }
 
-        const objectDetector = ml5.objectDetector('cocossd', modelLoaded)
-
-        const detect = () => {
-            if (webcamRef.current.video.readyState !== 4) {
-                console.warn('Video not ready yet')
-                return
-            }
-
-            objectDetector.detect(webcamRef.current.video, (err, results) => {
-                const ctx = canvasRef.current.getContext('2d')
-                ctx.clearRect(0, 0, width, height)
-                if (results && results.length) {
-                    results.forEach((detection) => {
-                        ctx.beginPath()
-                        ctx.fillStyle = '#FF0000'
-                        const { label, x, y, width, height } = detection
-                        ctx.fillText(label, x, y - 5)
-                        ctx.rect(x, y, width, height)
-                        ctx.stroke()
-                    })
-                }
-            })
-        }
 
         return () => {
             if (detectionInterval) {
                 clearInterval(detectionInterval)
             }
+
         }
     }, [width, height])
 
     return (
-        <>
+        <ErrorBoundary fallback={<p>Something went wrong</p>}>
             <Webcam ref={webcamRef} className="webcam" />
             <canvas ref={canvasRef} className="canvas" />
+            
 
             <button
                 type="button"
@@ -68,7 +91,7 @@ function App() {
             >
                 Default
             </button>
-        </>
+        </ErrorBoundary>
     )
 }
 

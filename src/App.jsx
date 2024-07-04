e6e812f65cd961dbe61f6f7721f1675ee1f843f0
Svitlana Rybakova <rsv0011@gmail.com>
2024-07-04 19:11:37 +0200

    feat: replace the canvas to p5

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
+import React, { useEffect, useRef, useState } from 'react'
 import Webcam from 'react-webcam'
+import p5 from 'p5'
 import './App.css'
-import { useEffect, useRef } from 'react'
 import ErrorBoundary from './components/ErrorBoundary'
-
 
 const dimensions = {
     width: 800,
@@ -13,78 +13,90 @@ function App() {
     const { width, height } = dimensions
     const detections = useRef([])
     const webcamRef = useRef()
-    const canvasRef = useRef()
- 
-
-    const draw = () => {
-        const ctx = canvasRef.current.getContext('2d')
-        ctx.clearRect(0, 0, width, height)
-        detections.current.forEach((detection) => {
-            ctx.beginPath()
-            ctx.fillStyle = '#FF0000'
-            ctx.fillText(detection.label, detection.x, detection.y - 5)
-            ctx.rect(
-                detection.x,
-                detection.y,
-                detection.width,
-                detection.height
-            )
-            ctx.stroke()
-        })
-    }
-
+    const sketchRef = useRef()
+    const [detectLoading, setDetectLoading] = useState('Start detecting...');
 
     const detect = (objectDetector) => {
-        console.log(webcamRef.current.video.readyState);
-        if (webcamRef.current.video.readyState !== 4) {
+        if (webcamRef.current?.video.readyState !== 4) {
             console.error('Video not ready yet')
             return
         }
 
+        setDetectLoading('LOADING');
+
         objectDetector.detect(webcamRef.current.video, (err, results) => {
             if (err) {
-                console.error('Error, detecting the video')
+                console.error('Error detecting the video', err)
+                setDetectLoading('ERROR');
                 return
             }
-
-            detections.current = results
-            draw()
+              if (results && results.length > 0) {
+                detections.current = results;
+                setDetectLoading('READY, detecting');
+            } else {
+                setDetectLoading('No results found');
+            }
+            
         })
+    }
+
+    const sketch = (p) => {
+
+        p.setup = () => {
+            p.createCanvas(width, height)
+        }
+
+        p.draw = () => {
+            p.clear()
+
+            detections.current.forEach((detection) => {
+                p.noFill()
+                p.stroke(21, 225, 43)
+                p.rect(
+                    detection.x,
+                    detection.y,
+                    detection.width,
+                    detection.height
+                )
+                p.textSize(16)
+                p.fill(0, 0, 0)
+                p.text(
+                    detection.label,
+                    detection.x,
+                    detection.y > 10 ? detection.y - 5 : 10
+                )
+            })
+        }
     }
 
     useEffect(() => {
         let detectionInterval
 
         const modelLoaded = () => {
-
-            webcamRef.current.video.width = width;
-            webcamRef.current.video.height = height;
-            canvasRef.current.width = width;
-            canvasRef.current.height = height;
-        }
-        const objectDetector = ml5.objectDetector('cocossd', modelLoaded)
-
-        if (modelLoaded) {
             detectionInterval = setInterval(() => {
                 detect(objectDetector)
-            }, 200)
+            }, 2000)
         }
 
+        const objectDetector = window.ml5.objectDetector('cocossd', modelLoaded)
+
+        const p5Instance = new p5(sketch, sketchRef.current)
 
         return () => {
             if (detectionInterval) {
                 clearInterval(detectionInterval)
             }
-
+            p5Instance.remove()
         }
     }, [width, height])
 
     return (
         <ErrorBoundary fallback={<p>Something went wrong</p>}>
-            <Webcam ref={webcamRef} className="webcam" />
-            <canvas ref={canvasRef} className="canvas" />
-            
-
+            <div>
+                <Webcam ref={webcamRef} className="webcam" />
+                <div ref={sketchRef} className="canvas"></div>
+            </div>
+    <p>{detectLoading}</p>
             <button
                 type="button"
                 className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"

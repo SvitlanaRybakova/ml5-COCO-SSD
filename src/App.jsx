31367b4308b8e7e6eb4da6d47ce9146a2196013d
Svitlana Rybakova <rsv0011@gmail.com>
2024-07-08 17:22:47 +0200

    fix: css styles

--- src/App.jsx
+++ src/App.jsx
@@ -115,41 +115,43 @@ function App() {
 
     return (
         <ErrorBoundary fallback={<p>Something went wrong</p>}>
-            <div className="h-[500px]">
-                {useCamera ? (
-                    <Webcam ref={webcamRef} className="webcam" />
-                ) : (
-                    <Player
-                        ref={playerRef}
-                        width={width}
-                        height={height}
-                        src={video}
-                        type={'video/mp4'}
-                        className="webcam"
+            <div className=" w-[60%] flex flex-col items-stretch">
+                <div className="h-[500px]">
+                    {useCamera ? (
+                        <Webcam ref={webcamRef} className="webcam" />
+                    ) : (
+                        <Player
+                            ref={playerRef}
+                            width={width}
+                            height={height}
+                            src={video}
+                            type={'video/mp4'}
+                            className="webcam"
+                        />
+                    )}
+                    <div ref={sketchRef} className="canvas" />
+                </div>
+
+                <div>
+                    <p className="text-black h-[40px]">{detectStatus}</p>
+                    <Button
+                        handleClick={handleButtonClickStart}
+                        disabled={isDetecting}
+                        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+                        btnText={'Start Detecting'}
                     />
-                )}
-                <div ref={sketchRef} className="canvas" />
-            </div>
 
-            <div>
-                <p className="text-black h-[40px]">{detectStatus}</p>
-                <Button
-                    handleClick={handleButtonClickStart}
-                    disabled={isDetecting}
-                    className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-                    btnText={'Start Detecting'}
-                />
+                    <Button
+                        handleClick={handleButtonClickStop}
+                        disabled={!isDetecting}
+                        className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
+                        btnText={'Stop Detecting'}
+                    />
 
-                <Button
-                    handleClick={handleButtonClickStop}
-                    disabled={!isDetecting}
-                    className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
-                    btnText={'Stop Detecting'}
-                />
-
-                <DetectionSourceToggle
-                    handleToggle={handleToggleDetectionSource}
-                />
+                    <DetectionSourceToggle
+                        handleToggle={handleToggleDetectionSource}
+                    />
+                </div>
             </div>
         </ErrorBoundary>
     )
